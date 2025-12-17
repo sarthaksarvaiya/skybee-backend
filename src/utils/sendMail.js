2140da2b9a1d40147e15c1b547dd681cbccfe895
Sarthak Sarvaiya <sarthaksarvaiya948@gmail.com>
2025-12-17 23:11:17 +0530

    Fix Mail

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,12 +4,16 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
+
 
 export const sendMail = async ({ name, email, phone, message }) => {
   await transporter.sendMail({
